c2a60be10f8c87c6d24ea1d84045277a743d69e3
Justin Dray <justin@dray.be>
2014-09-20 20:19:57 +1000

    Made it create the actual folders it should be linking

--- config-shared-documents.sh
+++ config-shared-documents.sh
@@ -2,7 +2,7 @@
 
 if [[ -d $1 ]] && [[ -d $1/drive_c ]]
 then
-	mkdir -p "$HOME/.PlayOnLinux/shared-files"
+	mkdir -p "$HOME/.PlayOnLinux/shared-files/"{'Application Data','Desktop','My Documents','My Music','My Pictures','My Videos'}
 	find "$1/drive_c/" -type l -exec sh -c 'rm "{}";ln -s "$HOME/.PlayOnLinux/shared-files/$(basename "{}")" "{}"' \;
 	rm -rf "$1/drive_c/users/$USER/Application Data"
 	ln -s "$HOME/.PlayOnLinux/shared-files/Application Data" "$1/drive_c/users/$USER/Application Data"
